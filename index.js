2620895898acbe3f7f59807e5b27e3e99052f879
Stefany <125682393+STEFANYALVES07@users.noreply.github.com>
2023-09-20 13:11:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('20/09/2023 Stefany Beatriz Alves');
+     var dataAtual = new Date();
+     var dataFormatada = dataAtual.toDateString();
+  res.end('Stefany Beatriz Alves!\nData atual: ' + dataFormatada);
 }).listen(8023);
